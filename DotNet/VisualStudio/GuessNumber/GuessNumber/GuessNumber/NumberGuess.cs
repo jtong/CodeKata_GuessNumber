ead43bfd59bde7c6f257d09f9d759b7ac95b67f0
Jian <notset@zynx.com>
2012-03-18 14:44:46 +0800

    Jian - #1 - Can generate judge string

---
+++ DotNet/VisualStudio/GuessNumber/GuessNumber/GuessNumber/NumberGuess.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace GuessNumber
+{
+    public class NumberGuess
+    {
+        public static string suggest(List<string> input, List<string> result)
+        {
+            
+            var aCount = getACount(input, result);
+            var sameCount = getBCount(input, result);
+            return string.Format("{0}A{1}B", aCount, sameCount - aCount);
+        }
+
+        private static int getBCount(List<string> input, List<string> result)
+        {
+            int SameCount = 0;
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (result.Contains(input[i]))
+                {
+                    SameCount++;
+                }
+            }
+
+            return SameCount;
+        }
+
+        private static int getACount(List<string> input, List<string>result )
+        {
+            int ACount = 0;
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i].Equals(result[i]))
+                {
+                    ACount++;
+                }
+            }
+            return ACount;
+        } 
+    }
+}
---
+++ DotNet/VisualStudio/GuessNumber/GuessNumber/GuessNumberTest/NumberGuessTest.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using GuessNumber;
+using Xunit;
+
+namespace GuessNumberTest
+{
+    public class NumberGuessTest
+    {
+        [Fact]
+        public void should_return_0A0B_given_result_numbers_have_no_same_number_with_input()
+        {
+            var result = new List<string>{ "5", "6", "7", "8"};
+            var input = new List<string>{ "1", "2", "3", "4"};
+
+            string expectSuggest = "0A0B";
+
+            string actualSuggest = NumberGuess.suggest(input, result);
+
+            Assert.Equal(expectSuggest, actualSuggest);
+        }
+
+        [Fact]
+        public void should_return_4A0B_given_result_numbers_are_exactly_same_with_input_numbers()
+        {
+            var result = new List<string> { "1", "2", "3", "4" };
+            var input = new List<string>{ "1", "2", "3", "4"};
+
+            string expectSuggest = "4A0B";
+
+            string actualSuggest = NumberGuess.suggest(input, result);
+
+            Assert.Equal(expectSuggest, actualSuggest);
+        }
+
+        [Fact]
+        public void should_1A_0B_given_result_numbers_has_one_number_exsit_in_input_numbers_and_in_the_same_place()
+        {
+            var result = new List<string> { "1", "5", "6", "7" };
+            var input = new List<string> { "1", "2", "3", "4" };
+
+            string expectSuggest = "1A0B";
+            
+            string actualSuggest = NumberGuess.suggest(input, result);
+
+            Assert.Equal(expectSuggest, actualSuggest);
+        }
+
+        [Fact]
+        public void should_return_0A4B_result_numbers_has_same_number_in_input_numbers_but_not_any_in_same_palce()
+        {
+
+            var result = new List<string> { "4", "3", "2", "1" };
+            var input = new List<string> { "1", "2", "3", "4" };
+
+            string expectSuggest = "0A4B";
+
+            string actualSuggest = NumberGuess.suggest(input, result);
+
+            Assert.Equal(expectSuggest, actualSuggest);
+        }
+
+        [Fact]
+        public void should_return_1A2B_result_numbers()
+        {
+
+            var result = new List<string> { "4", "3", "2", "1" };
+            var input = new List<string> { "4", "2", "3", "5" };
+
+            string expectSuggest = "1A2B";
+
+            string actualSuggest = NumberGuess.suggest(input, result);
+
+            Assert.Equal(expectSuggest, actualSuggest);
+        }
+
+    }
+}
